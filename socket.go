8386e2108df5bf6b89b3cfe7aa2a5948d1594581
François Kooman <fkooman@tuxed.net>
2019-08-06 22:47:37 +0200

    intial ugly stab at status

--- socket.go
+++ socket.go
@@ -76,43 +76,64 @@ func handleConnection(conn net.Conn) {
 			if len(msg) > 13 {
 				// we don't want to \n otherwise we could use msg[11:]
 				commonName = msg[11 : len(msg)-2]
-				break
+
+				c := make(chan bool, len(intPortList))
+
+				for _, p := range intPortList {
+					wg.Add(1)
+					go disconnectClient(c, p, commonName)
+				}
+
+				// wait for all routines to finish...
+				wg.Wait()
+
+				// close channel, we do not expect any data anymore, this is needed
+				// because otherwise "range c" below is still waiting for more data on the
+				// channel...
+				close(c)
+
+				// below we basically count all the "trues" in the channel populated by the
+				// routines...
+				clientDisconnectCount := 0
+				for b := range c {
+					if b {
+						clientDisconnectCount++
+					}
+				}
+
+				writer.WriteString(fmt.Sprintf("%d\n", clientDisconnectCount))
+				writer.Flush()
 			}
 		}
+
+		if 0 == strings.Index(msg, "LIST") {
+
+			fmt.Println("LIST")
+
+			c := make(chan []string, len(intPortList))
+
+			for _, p := range intPortList {
+				wg.Add(1)
+				go obtainStatus(c, p)
+			}
+
+			// wait for all routines to finish...
+			wg.Wait()
+
+			// close channel, we do not expect any data anymore, this is needed
+			// because otherwise "range c" below is still waiting for more data on the
+			// channel...
+			close(c)
+
+			for x := range c {
+				fmt.Println(x)
+			}
+		}
+
 		if 0 == strings.Index(msg, "QUIT") {
 			return
 		}
 	}
-
-	// XXX we have to make sure that commonName is actually filled, I guess
-	// we kinda do here by accident
-
-	c := make(chan bool, len(intPortList))
-
-	for _, p := range intPortList {
-		wg.Add(1)
-		go disconnectClient(c, p, commonName)
-	}
-
-	// wait for all routines to finish...
-	wg.Wait()
-
-	// close channel, we do not expect any data anymore, this is needed
-	// because otherwise "range c" below is still waiting for more data on the
-	// channel...
-	close(c)
-
-	// below we basically count all the "trues" in the channel populated by the
-	// routines...
-	clientDisconnectCount := 0
-	for b := range c {
-		if b {
-			clientDisconnectCount++
-		}
-	}
-
-	writer.WriteString(fmt.Sprintf("%d\n", clientDisconnectCount))
-	writer.Flush()
 }
 
 func disconnectClient(c chan bool, p int, commonName string) {
@@ -148,6 +169,50 @@ func disconnectClient(c chan bool, p int, commonName string) {
 		c <- true
 	} else {
 		c <- false
+	}
+
+	// XXX maybe it is easier to just close the connection, who cares about
+	// quit?
+	fmt.Fprintf(conn, "quit\n")
+}
+
+func obtainStatus(c chan []string, p int) {
+	defer wg.Done()
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", p))
+	if err != nil {
+		// unable to connect, no matter, maybe the process is temporary away,
+		// so no need to disconnect clients there ;-)
+		c <- nil
+		return
+	}
+
+	defer conn.Close()
+
+	// turn off live OpenVPN log that can confuse our output parsing
+	fmt.Fprintf(conn, fmt.Sprint("log off\n"))
+
+	reader := bufio.NewReader(conn)
+	// we need to remove everything that's currently in the buffer waiting to
+	// be read. We are not interested in it at all, we only care about the
+	// response to our commands hereafter...
+	// XXX there should be a one-liner that can fix this, right?
+	txt, _ := reader.ReadString('\n')
+	for 0 != strings.Index(txt, "END") && 0 != strings.Index(txt, "SUCCESS") && 0 != strings.Index(txt, "ERROR") {
+		txt, _ = reader.ReadString('\n')
+	}
+
+	// disconnect the client
+	fmt.Fprintf(conn, "status 2\n")
+	text, _ := reader.ReadString('\n')
+	for 0 != strings.Index(text, "CLIENT_LIST") {
+		// walk until we find CLIENT_LIST
+		text, _ = reader.ReadString('\n')
+	}
+
+	for 0 == strings.Index(text, "CLIENT_LIST") {
+		strList := strings.Split(text, ",")
+		c <- strList
 	}
 
 	// XXX maybe it is easier to just close the connection, who cares about
